74faf2f04f9b4c5c5a82fab45fdbdd27eb34433e
Jinho Ahn <jinoan89@gmail.com>
2023-08-03 16:19:14 +0900

    chore: create package setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kor_sen_splitter',
+    version='0.0.1',
+    description="한국어 문장 분리기",
+    url="https://github.com/oddbooks-ml/kor_sen_splitter.git",
+    author="jinoan",
+    author_email="jinoan89@gmail.com",
+    packages=["kor_sen_splitter"],
+    install_requires=["kss"]
+)
