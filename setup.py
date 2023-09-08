b5f8ae65cc911bde59a5d205279d47b9284a32a6
jinoan <jinoan89@gmail.com>
2023-09-08 11:19:26 +0900

    chore: v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kor_sen_splitter',
-    version='0.0.1',
+    version='0.0.2',
     description="한국어 문장 분리기",
     url="https://github.com/oddbooks-ml/kor_sen_splitter.git",
     author="jinoan",
