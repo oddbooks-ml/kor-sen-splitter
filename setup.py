0203eb7c1c9e0a00420e63e181404ff21f68ee2e
jinoan <jinoan89@gmail.com>
2023-11-17 15:06:06 +0900

    url 경로 수정

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='kor-sen-splitter',
     version='0.0.3',
     description="한국어 문장 분리기",
-    url="https://github.com/oddbooks-ml/kor_sen_splitter.git",
+    url="https://github.com/oddbooks-ml/kor-sen-splitter.git",
     author="jinoan",
     author_email="jinoan89@gmail.com",
     packages=["kor_sen_splitter"],
